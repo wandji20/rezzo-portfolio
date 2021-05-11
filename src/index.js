302c8c76061ca6c19480b5787f6b1a78263bc425
wandji20 <wbertrand10@gmail.com>
2021-05-11 03:53:14 +0100

    Add contact form and footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './style.css';
 import displayNav from './nav'
+import './../assets/zattix_store.png'
+import './../assets/culture.png'
+import './../assets/linters.png'
+import './../assets/todo.png'
+
 
 displayNav()
+const img1 = document.querySelector('.img1')
+img1.src = './../assets/culture.png'
+const img2 = document.querySelector('.img2')
+img2.src = './../assets/zattix_store.png'
+const img3 = document.querySelector('.img3')
+img3.src = './../assets/todo.png'
+const img4 = document.querySelector('.img4')
+img4.src = './../assets/linters.png'
+
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+
     ],
   },
 };
