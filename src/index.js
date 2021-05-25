8863d5788c9b36bf0dd1e6410fd93deb13ce55c7
wandji20 <wbertrand10@gmail.com>
2021-05-25 19:21:29 +0100

    Update skills images to resize for larger screen views

--- src/index.js
+++ src/index.js
@@ -13,19 +13,4 @@ import '../assets/js.jpg';
 
 require('bootstrap-icons/font/bootstrap-icons.css');
 
-// const img1 = document.querySelector('.img1');
-// img1.src = './../assets/culture.png';
-// const img2 = document.querySelector('.img2');
-// img2.src = './../assets/zattix_store.png';
-// const img3 = document.querySelector('.img3');
-// img3.src = './../assets/todo.png';
-// const img4 = document.querySelector('.img4');
-// img4.src = './../assets/linters.png';
-
-// document.querySelector('.bootstrap-img').src = './../assets/bootstrap.jpeg';
-// document.querySelector('.css-img').src = './../assets/css.png';
-// document.querySelector('.js-img').src = './../assets/js.jpg';
-// document.querySelector('.ruby-img').src = './../assets/ruby.jpeg';
-// document.querySelector('.html-img').src = './../assets/html.png';
-// document.querySelector('.ror-img').src = './../assets/rubyrails.png';
 start();
